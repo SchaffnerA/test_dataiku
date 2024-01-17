35b31330446bd57ef9a4964bbd8be788247c5ca0
adrien.schaffner@aqsone.com <adrien.schaffner@aqsone.com>
2024-01-17 16:54:06 +0000

    Update code_studioversioned bundle of test_streamlit_shap in TEST

---
+++ code_studios/X6Zop4e/streamlit/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+import dataiku
+import shap
+st.title('From gcs to streamlit using dataiku!')
+
+new_df = dataiku.Dataset("merge_col")
+new_df = new_df.get_dataframe()
+st.write("First five lines of the DataFrame:")
+st.write(new_df.head())
