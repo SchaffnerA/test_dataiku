819bc4f5ff7fa7cdc3559905dc837b442f6ab1ac
adrien.schaffner@aqsone.com <adrien.schaffner@aqsone.com>
2024-01-17 15:00:28 +0000

    Update code_studioversioned bundle of test_template_streamlit in TEST

--- code_studios/h8IYOYi/streamlit/app.py
+++ code_studios/h8IYOYi/streamlit/app.py
@@ -6,27 +6,7 @@ import dataiku
 
 st.title('Hello Streamlit!')
 
-# This loads dummy data into a dataframe
-df = pd.DataFrame({
-    'Trial A': np.random.normal(0, 0.8, 1000),
-    'Trial B': np.random.normal(-2, 1, 1000),
-    'Trial C': np.random.normal(3, 2, 1000)
-})
-
-# Uncomment the following to read your own dataset
-#dataset = dataiku.Dataset("YOUR_DATASET_NAME_HERE")
-#df = dataset.get_dataframe()
-
-c = alt.Chart(df).transform_fold(
-    ['Trial A', 'Trial B', 'Trial C'], 
-    as_=['Experiment', 'Measurement']
-).mark_bar(
-    opacity=0.3, 
-    binSpacing=0
-).encode(
-    alt.X('Measurement:Q', bin=alt.Bin(maxbins=100)), 
-    alt.Y('count()', stack=None), 
-    alt.Color('Experiment:N')
-)
-
-st.altair_chart(c, use_container_width=True)
+filtered = dataiku.Dataset("filtered")
+filtered_df = filtered.get_dataframe()
+st.write("First five lines of the DataFrame:")
+st.write(filtered_df.head())
